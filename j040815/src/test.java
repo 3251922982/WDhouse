b01e53a04c8ef28a26833a64e19fe649a4364a91
cloud-and-foolish-sea <3251922982@qq.com>
2024-08-15 15:57:33 +0800

    java测试提交

---
+++ j040815/src/test.java
@@ -0,0 +1,17 @@
+/**
+ *@version v1.0.0
+ *@author gaobo
+ * 作用helloword类 入门第一个程序练习
+ */
+public class helloword{
+    /**
+     *@param args [这是一个main函数]
+     */
+
+    //遍历数组	  args
+    public static void main(String[ ] args)  {
+        for(int i=0;    i < args.length;   i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
