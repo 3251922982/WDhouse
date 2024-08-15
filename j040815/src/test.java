206edbf034d02905c10573c2fea5eadd4e82e5cb
cloud-and-foolish-sea <3251922982@qq.com>
2024-08-15 18:22:05 +0800

    测试提交

--- j040815/src/test.java
+++ j040815/src/test.java
@@ -1,17 +1,36 @@
-/**
- *@version v1.0.0
- *@author gaobo
- * 作用helloword类 入门第一个程序练习
- */
-public class helloword{
-    /**
-     *@param args [这是一个main函数]
-     */
+import java.net.SocketOption;
 
-    //遍历数组	  args
-    public static void main(String[ ] args)  {
-        for(int i=0;    i < args.length;   i++) {
-            System.out.println(args[i]);
-        }
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+        int a;
+        a=100;
+        final int b;
+        b=666;
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+        double e=10.5;
+        float f=11.5f;
+        System.out.println(e*e);
+        System.out.println(f);
+        char ch=97;
+        System.out.println(ch);
+        boolean g=true;
+        System.out.println(g);
+        int a1=20;
+        long a2=10;
+        a2=a1;
+        System.out.println(a2);
+
+
+        String str ="王东";
+        String str2="hello";
+        System.out.println("输出："+str  +str2+a);//打印为字符串
+
     }
 }
+
